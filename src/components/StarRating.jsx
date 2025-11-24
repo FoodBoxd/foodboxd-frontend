be0ebb2157165a338d1928fdfeaac8db8e675d5a
Bernardo <bernardopcandido@gmail.com>
2025-11-24 02:01:11 -0300

    fix: remove comments

--- src/components/StarRating.jsx
+++ src/components/StarRating.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './StarRating.css';
 
-// Componente para Estrela Cheia
 const FullStar = () => (
   <svg className="star-svg" viewBox="0 0 24 24" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
     <path d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21 12 17.27z"/>
@@ -25,16 +24,12 @@ const EmptyStar = () => (
   </svg>
 );
 
-// Componente principal que renderiza as estrelas com base na nota
 export default function StarRating({ score }) {
   const stars = [];
-  // Loop de 1 a 5 para criar as estrelas
   for (let i = 1; i <= 5; i++) {
     if (i <= score) {
-      // Se a nota for maior ou igual ao índice, estrela cheia
       stars.push(<FullStar key={i} />);
     } else {
-      // Senão, estrela vazia
       stars.push(<EmptyStar key={i} />);
     }
   }
